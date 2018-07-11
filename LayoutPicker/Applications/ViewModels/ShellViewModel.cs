d9fa0a3be07979097649fd93a189bec0895a8c17
Mica Funston <mica.funston@gmail.com>
2018-07-10 20:00:29 -0400

    Remove unecessary code from ViewModel

--- LayoutPicker/Applications/ViewModels/ShellViewModel.cs
+++ LayoutPicker/Applications/ViewModels/ShellViewModel.cs
@@ -13,41 +13,15 @@ namespace LayoutPicker.Applications.ViewModels
     {
         private readonly DelegateCommand exitCommand;
         public readonly SettingsHandler settingsHandler = new SettingsHandler();
-        public LayoutChoices layoutChoices = new LayoutChoices();
-        public List<string> PageSizeValues { get; set; }
-        public List<string> BindingTypeValues { get; set; }
-        public List<string> UpsValues { get; set; }
-        public List<string> HeadToHeadValues { get; set; }
-        public List<string> HeadTrimValues { get; set; }
-        public List<string> SheetSizeValues { get; set; }
-        public List<string> CaliperValues { get; set; }
-        public List<string> PageCountValues { get; set; }
-        public List<string> PressValues { get; set; }
-        public List<string> MultiplierValues { get; set; }
-        public List<string> SignatureValues { get; set; }
         public ObservableCollection<LayoutItem> Layout { get; set; }
         [ImportingConstructor]
         public ShellViewModel(IShellView view)
             : base(view)
         {
-            //Layout = new List<LayoutItem>();
-            settingsHandler.GetLayoutOptions();
-            layoutChoices.GetLayoutChoices(settingsHandler);
-            PageSizeValues = layoutChoices.PageSizeValues;
-            BindingTypeValues = layoutChoices.BindingTypeValues;
-            UpsValues = layoutChoices.UpsValues;
-            HeadToHeadValues = layoutChoices.HeadToHeadValues;
-            HeadTrimValues = layoutChoices.HeadTrimValues;
-            SheetSizeValues = layoutChoices.SheetSizeValues;
-            CaliperValues = layoutChoices.CaliperValues;
-            PageCountValues = layoutChoices.PageCountValues;
-            PressValues = layoutChoices.PressValues;
-            MultiplierValues = layoutChoices.MultiplierValues;
-            SignatureValues = layoutChoices.SignatureValues;
+            settingsHandler.GetLayoutOptions();            
             LayoutFactory layoutFactory = new LayoutFactory();
             Layout = new ObservableCollection<LayoutItem>(layoutFactory.MakeLayout(settingsHandler));
-            exitCommand = new DelegateCommand(Close);
-            
+            exitCommand = new DelegateCommand(Close);            
         }
 
 
