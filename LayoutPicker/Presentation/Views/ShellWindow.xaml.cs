917075988d519a16ffde5045fadd84e2c7da2d2d
Mica Funston <mica.funston@gmail.com>
2018-07-29 21:26:17 -0400

    UI Changes

--- LayoutPicker/Presentation/Views/ShellWindow.xaml.cs
+++ LayoutPicker/Presentation/Views/ShellWindow.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.Composition;
 using System.Windows;
 using LayoutPicker.Applications.Views;
+using MahApps.Metro.Controls;
 
 namespace LayoutPicker.Presentation.Views
 {
     [Export(typeof(IShellView))]
-    public partial class ShellWindow : Window, IShellView
+    public partial class ShellWindow : MetroWindow, IShellView
     {
         public ShellWindow()
         {
